350c55f6d635e849fdc53a03615249ff1e46ccf3
Nior <52194934+Nior21@users.noreply.github.com>
2023-12-23 20:32:02 +0300

    Add custom cursor class

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cursor":return img`
+2 . 2 2 2 2 2 2 2 2 2 2 2 2 . 2 
+. . . . . . . . . . . . . . . . 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . . 
+2 . 2 2 2 2 2 2 2 2 2 2 2 2 . 2 
+`;
+            case "image2":
+            case "info":return img`
+2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (controller.A.isPressed()) {
-    	
+        cursor.controller_handler('up')
     } else {
         controller.moveSprite(mySprite, 100, 0)
         simplified.gravity_jump(mySprite, -200)
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controller.A.isPressed()) {
+        cursor.controller_handler('down')
     }
 })
 function random (min: number, max: number) {
@@ -21,7 +26,7 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     if (controller.A.isPressed()) {
-    	
+        cursor.controller_handler('left')
     } else {
         controller.moveSprite(mySprite, 100, 0)
         isRight = false
@@ -29,7 +34,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (controller.A.isPressed()) {
-    	
+        cursor.controller_handler('right')
     } else {
         controller.moveSprite(mySprite, 100, 0)
         isRight = true
@@ -227,3 +232,89 @@ isRight = true
 генераторБлоков(assets.tile`myTile14`, 1, 51, 51)
 режим = ["ставитьБлоки", "долбить"]
 текущийРежим = 0
+
+let rowOfTiles: Image[] = []
+let allTiles: Image[][] = []
+for (let y = 0; y <= 15; y++) {
+    rowOfTiles = []
+    // console.log(rowOfTiles[x]);
+    for (let x = 0; x <= 15; x++) {
+        rowOfTiles[x] = tiles.tileImageAtLocation(tiles.getTileLocation(x, y))
+    }
+    allTiles[y] = rowOfTiles
+}
+// устанавливает исходную картинку как самый левый верхний тайл
+let stastic: Sprite = new Sprite(allTiles[0][0]);
+class InfoSprite {
+    sprite: Sprite;
+    constructor() {
+        this.sprite = new Sprite(assets.image`info`);
+        this.sprite.setPosition(0, 104)
+    }
+}
+class Cursor extends Sprite {
+    current_aim: object;
+    sensitivity: number;
+    infoSprite: InfoSprite;
+    constructor(image: Image, sensitivity: number) {
+        super(image);
+        this.sensitivity = sensitivity;
+        this.infoSprite = new InfoSprite();
+        this.infoSprite.sprite.setPosition(80, 112)
+    }
+
+    controller_handler(direction: string) {
+        switch (direction) {
+            case 'left':
+                if (this.x > 8) {
+                    this.x += this.sensitivity * -1
+                }
+                break;
+            case 'right':
+                if (this.x <= 15 * 16) {
+                    this.x += this.sensitivity * 1
+                }
+                break;
+            case 'up':
+                if (this.y > 8) {
+                    this.y += this.sensitivity * -1
+                }
+                break;
+            case 'down':
+                if (this.y <= 15 * 16) {
+                    this.y += this.sensitivity * 1
+                }
+                break;
+        }
+        //console.log(`x: ${cursor.tilemapLocation().column}, y: ${cursor.tilemapLocation().row}`);
+
+        let tile = tiles.getTileLocation(
+            cursor.tilemapLocation().column, cursor.tilemapLocation().row
+        )
+        //console.log(tiles.tileAtLocationEquals(cursor.tilemapLocation(), allTiles[1][1]));
+        //console.log(tile.x);
+        //console.log(tile.y);
+
+        const ppx = tile.x + 16 + 8;
+        const ppy = tile.y + 16 + 8;
+        //console.log(cursor.tilemapLocation().column)
+        //console.log(cursor.tilemapLocation().row)
+        //console.log(allTiles[cursor.tilemapLocation().column][cursor.tilemapLocation().row])
+        let oldImage = allTiles[cursor.tilemapLocation().row][cursor.tilemapLocation().column]
+        let newImage = oldImage.clone();
+
+        stastic.setImage(newImage);
+        newImage.drawRect(0, 0, 16, 16, 1);
+
+        stastic.setPosition(ppx, ppy);
+
+        let x2 = scene.cameraProperty(CameraProperty.X)
+        let y2 = scene.cameraProperty(CameraProperty.Y)
+        this.infoSprite.sprite.setPosition(x2, y2 - 60 + 112)
+    }
+}
+let cursor = new Cursor(assets.image`cursor`, 16);
+cursor.setPosition(8, 8)
+scene.cameraFollowSprite(cursor)
+let width = screen.width;
+let height = screen.height;
